e32ec0c3131554e1951a5fa511c1675b67fa384f
vishusaxena <vishusaxena3105@gmail.com>
2024-05-04 17:12:04 +0530

    implemented Authentication

---
+++ controllers/middleware/auth.js
@@ -0,0 +1,17 @@
+const { getUser } = require("../service/auth");
+
+async function restrictToLoggedInUser(req,res,next){
+   
+    const userId=req.cookies?.uid;
+  
+    if(!userId)return res.redirect('/login');
+    
+    const user=getUser(userId);
+
+    if(!user)return res.redirect('/login');
+    req.user=user;
+next();
+}
+module.exports={
+    restrictToLoggedInUser
+}
