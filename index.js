694c87948888eeae64ddc686dd8090d891f84f04
vishusaxena <vishusaxena3105@gmail.com>
2025-02-17 19:27:28 +0530

    connected to atlas

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-const express = require('express');
-const path = require('path');
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
 const urlRoute = require("./routes/url");
-const staticRoute=require('./routes/staticRoutes');
-const userRoute=require('./routes/user');
+const staticRoute = require("./routes/staticRoutes");
+const userRoute = require("./routes/user");
 const { connectToMongoDB } = require("./connect");
-const cookieParser = require('cookie-parser');
-const { restrictToLoggedInUser,checkAuth } = require('./controllers/middleware/auth');
+const cookieParser = require("cookie-parser");
+const {
+  restrictToLoggedInUser,
+  checkAuth,
+} = require("./controllers/middleware/auth");
+
 const app = express();
-const PORT = 8000;
-connectToMongoDB('mongodb://localhost:27017/short-url').then(() => console.log(`mongoDB is connected`));
-app.set('view engine' ,'ejs');
-app.set("views",path.resolve('./views'));
+const PORT = process.env.PORT || 8000;
+
+connectToMongoDB(process.env.MONGO_URI).then(() =>
+  console.log(`MongoDB is connected`)
+);
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({extended:false}));
-app.use('/url',restrictToLoggedInUser, urlRoute);
-app.use('/user',userRoute);
-app.use('/',checkAuth,staticRoute);
-app.listen(PORT, () => { console.log(`server started at ${PORT}`) });
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/url", restrictToLoggedInUser, urlRoute);
+app.use("/user", userRoute);
+app.use("/", checkAuth, staticRoute);
+
+app.listen(PORT, () => {
+  console.log(`Server started at ${PORT}`);
+});
